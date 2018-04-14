4c996720519b77dba4ef504d58eb5221ae31e6f0
ShevT <evgeny.shishkov@gmail.com>
2018-04-14 17:34:44 +0300

    Fix copying files
    (cherry picked from commit 81874a1f85c1620a1ef6ea8f4e925302573dee3a)

--- MiuiCamera-vendor.mk
+++ MiuiCamera-vendor.mk
@@ -12,4 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/xiaomi/MiuiCamera_prebuild/system,system)
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/MiuiCamera/system/vendor/lib64/libmorpho_group_portrait.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmorpho_group_portrait.so \
+    vendor/xiaomi/MiuiCamera/system/etc/age_gender_bg:system/etc/age_gender_bg \
+    vendor/xiaomi/MiuiCamera/system/etc/android_model_facea.dat:system/etc/android_model_facea.dat \
+    vendor/xiaomi/MiuiCamera/system/etc/android_model_faceg.dat:system/etc/android_model_faceg.dat \
+    vendor/xiaomi/MiuiCamera/system/etc/face_goodly_208_180:system/etc/face_goodly_208_180 \
+    vendor/xiaomi/MiuiCamera/system/etc/face_ravishing_238_224:system/etc/face_ravishing_238_224 \
+    vendor/xiaomi/MiuiCamera/system/etc/face_splendid_274_200:system/etc/face_splendid_274_200 \
+    vendor/xiaomi/MiuiCamera/system/etc/lf_facerank_model.bin:system/etc/lf_facerank_model.bin \
+    vendor/xiaomi/MiuiCamera/system/etc/people_gender.dat:system/etc/people_gender.dat
