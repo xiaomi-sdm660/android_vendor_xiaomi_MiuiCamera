3379a97ca2e4c8e02881dfd8f36baba79ce8c522
ShevT <shev_t@mail.ru>
2018-05-15 21:05:10 +0300

    Android.mk: Device Filtering

--- Android.mk
+++ Android.mk
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter sagit chiron,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles)
+endif
