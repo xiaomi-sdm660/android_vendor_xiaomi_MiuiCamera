befa81105e9e6c9c8e9d7a1151fd63a5e6d46f39
rebenok90x <pokemonnekitgod@gmail.com>
2020-01-20 09:05:12 +0000

    Add Redmi Note 7 support
    Signed-off-by: Zainudin Shamilov <izainudinshamilov@beatwo.men>

--- config.mk
+++ config.mk
@@ -15,11 +15,7 @@
 VENDOR_PATH := vendor/xiaomi/MiuiCamera
 
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/system/etc/device_features/whyred.xml:system/etc/device_features/whyred.xml \
-    $(VENDOR_PATH)/system/etc/device_features/wayne.xml:system/etc/device_features/wayne.xml \
-    $(VENDOR_PATH)/system/etc/device_features/clover.xml:system/etc/device_features/clover.xml \
-    $(VENDOR_PATH)/system/etc/device_features/jasmine.xml:system/etc/device_features/jasmine.xml \
-    $(VENDOR_PATH)/system/etc/device_features/jasmine.xml:system/etc/device_features/jasmine_sprout.xml \
+    $(VENDOR_PATH)/system/etc/device_features/lavender.xml:system/etc/device_features/lavender.xml \
     $(VENDOR_PATH)/system/etc/default-permissions/miuicamera-permissions.xml:system/etc/default-permissions/miuicamera-permissions.xml \
     $(VENDOR_PATH)/system/etc/permissions/miuicamera-privapp-permissions.xml:system/etc/permissions/miuicamera-privapp-permissions.xml \
     $(VENDOR_PATH)/system/lib/libCameraEffectJNI.so:system/lib/libCameraEffectJNI.so \
