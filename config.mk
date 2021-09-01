7ffdcc0cac098c9c4dad1583a801c82990726c4a
IMMANUEL44 <immanuelr44@gmail.com>
2021-09-01 08:55:42 +0530

    Add soong namespace

--- config.mk
+++ config.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/MiuiCamera
+
 VENDOR_PATH := vendor/xiaomi/MiuiCamera
 
 PRODUCT_COPY_FILES += \
